21b4a4f758faae40c157e74030d3b32063a97e37
Min Jae (Chris) Lee <minjaelee0727@gmail.com>
2021-10-05 19:28:06 -0400

    feat: go brower that can add blockchain

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/minjaelee0727/gocoin
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         `json:"url"`,
+			Method:      `json:"method"`,
+			Description: `json:"description"`,
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
+
+func main() {
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
